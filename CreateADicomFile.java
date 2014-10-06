51011a89a6a856414a1976ba46eb3eb29fa6ea09
SaravananSubramanian <saravanansubramanianlaptop@Saravanan-MacBook.local>
2014-10-06 00:21:00 -0600

    More changes

--- CreateADicomFile.java
+++ CreateADicomFile.java
@@ -2,24 +2,21 @@ package com.saravanansubramanian.dicom.pixelmedtutorial;
 
 import com.pixelmed.dicom.AttributeList;
 import com.pixelmed.dicom.ImageToDicom;
-import com.pixelmed.dicom.SOPClass;
 
 public class CreateADicomFile {
 	
 	public static void main(String[] args) {
 		String scJpegFilePath = "/Users/saravanan/PixelMedTutorial/ChestGeneralImage.jpg";
 		String newDicomFile = "/Users/saravanan/PixelMedTutorial/Saravanan.dcm";
-        try {
+		try {
         	//generate the DICOM file from the jpeg file and the other attributes supplied
-			new ImageToDicom(scJpegFilePath, //path to secondary capture image 
+			new ImageToDicom(scJpegFilePath, //path to existing JPEG image 
 					         newDicomFile, //output DICOM file with full path
 					         "Saravanan Subramanian", //name of patient
 					         "12121221", //patient id
 					         "2323232322", //study id
 					         "3232323232", //series number
-					         "42423232234", //instance number
-					          "CT", //modality
-					          SOPClass.CTImageStorage); //SOP class
+					         "42423232234"); //instance number
 		    //now, dump the contents of the DICOM file to the console
 			AttributeList list = new AttributeList();
     		list.read(newDicomFile);
