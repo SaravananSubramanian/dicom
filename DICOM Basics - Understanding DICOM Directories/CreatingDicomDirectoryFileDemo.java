e9bf5e4ff6cffceb8920dd7bf6436146f7576931
SaravananSubramanian <saravanansubramanianlaptop@Saravanan-MacBook.local>
2014-10-18 19:47:27 -0600

    Code update

--- DICOM Basics - Understanding DICOM Directories/CreatingDicomDirectoryFileDemo.java
+++ DICOM Basics - Understanding DICOM Directories/CreatingDicomDirectoryFileDemo.java
@@ -1,5 +1,6 @@
 package com.saravanansubramanian.dicom.pixelmedtutorial;
 
+import java.io.File;
 import com.pixelmed.dicom.DicomDirectory;
 import com.pixelmed.dicom.DicomDirectoryBrowser;
 
@@ -7,11 +8,14 @@ public class CreatingDicomDirectoryFileDemo {
 	
 public static void main(String[] args) {
 		
-		String dicomDirectoryFileName = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo/DICOMDIR";
-		String imageFile1 = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo/MR3_UNC";
-		String imageFile2 = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo/CT1_RLE";
-		String imageFile3 = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo/CT2_RLE";
-		String imageFile4 = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo/VL3_UNC";
+	    String dicomRootDirectory = "/Users/Saravanan/PixelMedTutorial/RandomImagesForDicomDirectoryCreationDemo";
+		String dicomDirectoryFileName = dicomRootDirectory + File.separatorChar + "DICOMDIR";
+		//These are the image files that we will include as file-set
+		//remember, they may be under different directories such as patient, study & series folders usually
+		String imageFile1 = dicomRootDirectory + File.separatorChar + "MR3_UNC";
+		String imageFile2 = dicomRootDirectory + File.separatorChar + "CT1_RLE";
+		String imageFile3 = dicomRootDirectory + File.separatorChar + "CT2_RLE";
+		String imageFile4 = dicomRootDirectory + File.separatorChar + "VL3_UNC";
 		
 		try {
 			String[] sourceFiles = new String[] {imageFile1, imageFile2, imageFile3, imageFile4};
