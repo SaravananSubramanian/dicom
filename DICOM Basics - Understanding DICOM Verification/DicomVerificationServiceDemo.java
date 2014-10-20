6fe4f13dfaefe90a7ede6dcf0822ecd323fe562e
SaravananSubramanian <saravanansubramanianlaptop@Saravanan-MacBook.local>
2014-10-20 17:01:33 -0600

    Code update

--- DICOM Basics - Understanding DICOM Verification/DicomVerificationServiceDemo.java
+++ DICOM Basics - Understanding DICOM Verification/DicomVerificationServiceDemo.java
@@ -6,21 +6,20 @@ public class DicomVerificationServiceDemo {
 	
 	public static void main(String[] args) {
 		try {
-			//Demonstration of code to verify connectivity against David Clunie's public server
-			//hosted on the Amazon cloud
+			//Demonstration of code to verify connectivity against David Clunie's public server hosted on the Amazon cloud
 			
-			String hostname = "184.73.255.26"; //their hostname or IP address
-			int port = 11112; //the port their entity is listening on
+			String remoteEntityHostName = "184.73.255.26"; //their hostname or IP address
+			int remoteEntityPort = 11112; //the port their entity is listening on
 			String calledAETitle = "AWSPIXELMEDPUB"; //their Application Entity Title
 			String callingAETitle = "LOCAL"; //our Application Entity Title
 			boolean secureTransport = false; //optional - we wont need to use it for our example 
-			int debugLevel = 2; // zero for no debugging messages, higher values more verbose messages
+			int debugLevel = 2; // zero for no debugging messages, higher values for more verbose messages
 			
 			//Call the constructor for this class passing the various arguments whose values are listed above
-		    new VerificationSOPClassSCU(hostname,port, calledAETitle, callingAETitle, secureTransport, debugLevel);
+		    new VerificationSOPClassSCU(remoteEntityHostName,remoteEntityPort, calledAETitle, callingAETitle, secureTransport, debugLevel);
 		}
 		catch (Exception e) {
-		    e.printStackTrace();
+		    e.printStackTrace(); //in real life, do something about this exception
 		}
 	}
 
