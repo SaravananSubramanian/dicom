7ff1ba9fce4e58ed5896752c0c66c84491604b00
Saravanan Subramanian <SaravananSubramanian@users.noreply.github.com>
2018-06-19 00:03:24 -0600

    Updated example

--- DICOM Basics - Understanding DICOM Verification/DicomVerificationServiceDemo.java
+++ DICOM Basics - Understanding DICOM Verification/DicomVerificationServiceDemo.java
@@ -11,12 +11,12 @@ public class DicomVerificationServiceDemo {
 			String remoteEntityHostName = "184.73.255.26"; //their hostname or IP address
 			int remoteEntityPort = 11112; //the port their entity is listening on
 			String calledAETitle = "AWSPIXELMEDPUB"; //their Application Entity Title
-			String callingAETitle = "LOCAL"; //our Application Entity Title
+			String callingAETitle = "JAVACLIENT"; //our Application Entity Title
 			boolean secureTransport = false; //optional - we wont need to use it for our example 
 			int debugLevel = 2; // zero for no debugging messages, higher values for more verbose messages
 			
 			//Call the constructor for this class passing the various arguments whose values are listed above
-		    new VerificationSOPClassSCU(remoteEntityHostName,remoteEntityPort, calledAETitle, callingAETitle, secureTransport, debugLevel);
+		    new VerificationSOPClassSCU(remoteEntityHostName,remoteEntityPort, calledAETitle, callingAETitle, secureTransport,2);
 		}
 		catch (Exception e) {
 		    e.printStackTrace(); //in real life, do something about this exception
